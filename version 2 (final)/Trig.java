fd75daaa59000ccae5023231c8b291f83a9a2f3e
Faisal Bagalagel <uultradude@gmail.com>
2021-02-23 23:52:23 -0500

    added coments

--- version 2 (final)/Trig.java
+++ version 2 (final)/Trig.java
@@ -3,47 +3,70 @@
 
 public class Trig {
 
+    /**
+     * Calculates the sine function using the taylor polynomial equation,
+     * @param x input to go through the sine function
+     * @return function file
+     */
     public double sine_func(double x){
         double output = 0;
-        x = x % (2 * 3.14159265359);
+        x = x % (2 * 3.14159265359); //makes sure value is in the right range
 
-        for(int k = 0; k<10; k++){
+        for(int k = 0; k<10; k++){ //go through 10 terms
             double value = 2*k+1;
             output += (power(-1, k)/(factorial(2*k+1))) * power(x, (value));
-            //System.out.println(2*k+1);
         }
-
         return output;
     }
 
+    /**
+     * Calculates the cos function using the taylor polynomial equation,
+     * @param x input to go through the cos function
+     * @return function file
+     */
     public double cos_func(double x){
 
-        if(x == 0){
+        if(x == 0){ //special case if input is 0
             return 1;
         }
 
-        double output = 0;
-        x = x % (2 * 3.14159265359);
+        double output = 1;
+        x = x % (2 * 3.14159265359); //makes sure value is in range
 
-        for(int k = 0; k<10; k++){
+        for(int k = 0; k<10; k++){ //go through 10 terms
             double value = 2*k;
-            output += (power(-1, k)/(factorial(2*k))) * power(x, (value));
+            output += ((power(-1, k) * power(x, (value))/(factorial(2*k))));
         }
 
         return output;
     }
 
+    /**
+     * Calculates the tan function using the taylor polynomial equation,
+     * @param x input to go through the tan function
+     * @return function file
+     */
     public double tan_func(double x){
-        double output = sine_func(x)/cos_func(x);
+        double output = sine_func(x)/cos_func(x); //simply divide sin by cos to get the tan value
         return output;
     }
 
+    /**
+     * Converts degree value to radian
+     * @param x degree value to radian
+     * @return returns
+     */
     public static double convert_to_Radians(double x){
         double output = x * (3.14159265358979323846/180);
         return output;
 
     }
 
+    /**
+     * gets the factorial of a number
+     * @param n number to get factorial for
+     * @return
+     */
     public static long factorial(long n) {
         if (n <= 2) {
             return n;
@@ -51,10 +74,15 @@ public class Trig {
         return n * factorial(n - 1);
     }
 
-
-    public double power(double base, double power){
-        if(power == 0) return 1;
-        return base * power(base, --power);
+    /**
+     * calculates the value the first number to the power of the second
+     * @param a base value
+     * @param b power value
+     * @return
+     */
+    public double power(double a, double b){
+        if(b == 0) return 1;
+        return a * power(a, --b);
     }
 
 }
